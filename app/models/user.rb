b215c52ea8e4da264f0c2ecc52f11342b7491779
allezz <aerbay@gmail.com>
2020-08-20 00:13:51 +0200

    Add helper method to use model, and user random generate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
-    validates_uniqueness_of :username
+  validates_uniqueness_of :username
+
+  helper_method :current_user # it will make it available everywhere
+
+  def self.generate
+    adjectives = %w[Ancient Timid Creative Dangerous Effective Flying Dilligent]
+    nouns = %w[Highway Intern Jacket Fox Monkey Halberd]
+    number = rand.to_s[2..4]
+    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(username: username)
+  end
 end
