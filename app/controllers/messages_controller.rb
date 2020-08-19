bbdb3f22cfd78bc121f8b490e754702acc7d6b14
allezz <aerbay@gmail.com>
2020-08-20 00:03:04 +0200

    Add current_user to messages controller and application controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: %i[show edit update destroy]
 
   # GET /messages
   # GET /messages.json
@@ -9,8 +9,7 @@ class MessagesController < ApplicationController
 
   # GET /messages/1
   # GET /messages/1.json
-  def show
-  end
+  def show; end
 
   # GET /messages/new
   def new
@@ -18,13 +17,13 @@ class MessagesController < ApplicationController
   end
 
   # GET /messages/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /messages
   # POST /messages.json
   def create
     @message = Message.new(message_params)
+    @message.user = current_user
 
     respond_to do |format|
       if @message.save
@@ -62,13 +61,14 @@ class MessagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def message_params
-      params.require(:message).permit(:content, :user_id, :room_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def message_params
+    params.require(:message).permit(:content, :user_id, :room_id)
+  end
 end
