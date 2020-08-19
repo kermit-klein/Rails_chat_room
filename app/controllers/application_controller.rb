8831c54d4a63b49f4605e48e540d603dfd21faae
allezz <aerbay@gmail.com>
2020-08-20 00:25:55 +0200

    Fix current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user # it will make it available everywhere
+
   def current_user
     return @current_user if @current_user.present?
 
@@ -7,6 +9,7 @@ class ApplicationController < ActionController::Base
     else
       @current_user = User.generate
       session[:user_id] = @current_user.id
+      @current_user
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
 
-  helper_method :current_user # it will make it available everywhere
-
   def self.generate
     adjectives = %w[Ancient Timid Creative Dangerous Effective Flying Dilligent]
     nouns = %w[Highway Intern Jacket Fox Monkey Halberd]
