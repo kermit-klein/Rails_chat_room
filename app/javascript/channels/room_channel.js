102909de217c6335ab71a783cb2cc48d13797915
allezz <aerbay@gmail.com>
2020-09-02 01:11:37 +0200

    Mine nad theirs message work around

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,7 +1,7 @@
 import consumer from "./consumer";
 
 consumer.subscriptions.create(
-  { channel: "RoomChannel", room_id: 2 },
+  { channel: "RoomChannel", room_id: 1 },
   {
     connected() {
       console.log("connected");
@@ -13,6 +13,20 @@ consumer.subscriptions.create(
 
     received(data) {
       console.log(data);
+
+      const element = document.getElementById("user-id");
+      const user_id = Number(element.getAttribute("data-user-id"));
+
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.mine;
+      } else {
+        html = data.theirs;
+      }
+
+      const messageContainer = document.getElementById("messages");
+      messageContainer.innerHTML = messageContainer.innerHTML + html;
     },
   }
 );
--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -2,10 +2,16 @@ class SendMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    html = ApplicationController.render(
-      partial: 'messages/message',
+    mine = ApplicationController.render(
+      partial: 'messages/mine',
       locals: { message: message }
     )
-    ActionCable.server.broadcast "room_channel_#{message.room_id}", html: html
+
+    theirs = ApplicationController.render(
+      partial: 'messages/theirs',
+      locals: { message: message }
+    )
+
+    ActionCable.server.broadcast "room_channel_#{message.room_id}", mine: mine, theirs: theirs, message: message
   end
 end
