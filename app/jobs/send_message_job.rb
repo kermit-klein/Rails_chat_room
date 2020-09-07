32a358afd5fa0acf695c1cbfd53fc9ea34b06ca7
allezz <aerbay@gmail.com>
2020-09-07 23:40:47 +0200

    Refactor job and room channel.rb

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -18,12 +18,26 @@ class SendMessageJob < ApplicationJob
       locals: { message: message }
     )
 
-    cable_ready["room_channel_#{message.room_id}"].insert_adjacent_html(
-      selector: '#messages',
-      position: 'beforeend',
-      html: html,
-      user_id: message.user_id
-    )
-    cable_ready.broadcast
+    room = message.room
+    room.current_user.each do |user|
+      next if message.sent_by?(user)
+
+      stream = "room_#{message.room_id}_user_#{user.id}"
+
+      cable_ready[stream].insert_adjacent_html(
+        selector: '#messages',
+        position: 'beforeend',
+        html: html
+      )
+      cable_ready.broadcast
+    end
+
+    # cable_ready["room_channel_#{message.room_id}"].insert_adjacent_html(
+    #   selector: '#messages',
+    #   position: 'beforeend',
+    #   html: html,
+    #   user_id: message.user_id
+    # )
+    # cable_ready.broadcast
   end
 end
